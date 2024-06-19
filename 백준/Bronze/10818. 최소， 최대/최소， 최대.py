cf4e63f45a2024e68b72e2b770a8ebd1ae8930b8
songga0 <125133189+songga0@users.noreply.github.com>
2024-06-20 05:35:06 +0900

    [Bronze III] Title: 최소, 최대, Time: 564 ms, Memory: 144340 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.py
@@ -0,0 +1,14 @@
+n=int(input())
+
+a=list(map(int,input().split()))
+
+min=a[0]
+max=a[0]
+
+for i in range(n):
+    if a[i]<min:
+        min=a[i]
+    if a[i]>max:
+        max=a[i]
+        
+print(min, max)
