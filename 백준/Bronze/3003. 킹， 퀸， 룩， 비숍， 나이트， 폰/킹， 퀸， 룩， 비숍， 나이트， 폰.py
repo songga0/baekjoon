0b11ecd932f7a6afa556960a30a29c8827a3f7a8
songga0 <125133189+songga0@users.noreply.github.com>
2024-07-13 14:31:46 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -0,0 +1,31 @@
+a,b,c,d,e,f=map(int,input().split())
+
+if(a==1):
+    print('0',end=" ")
+else:
+    print(1-a,end=" ")
+    
+if(b==1):
+    print('0',end=" ")
+else:
+    print(1-b,end=" ")
+
+if(c==2):
+    print('0',end=" ")
+else:
+    print(2-c,end=" ")
+
+if(d==2):
+    print('0',end=" ")
+else:
+    print(2-d,end=" ")
+
+if(e==2):
+    print('0',end=" ")
+else:
+    print(2-e,end=" ")
+
+if(f==8):
+    print('0',end=" ")
+else:
+    print(8-f,end=" ")
