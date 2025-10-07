de6a922f39f2fcad791dab7a9bb1adb383b53a95
songga0 <125133189+songga0@users.noreply.github.com>
2025-10-07 15:29:52 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 118.37 ms, Memory: 27.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,9 @@
+def solution(arr):
+    answer = []
+    for i in arr:
+        if len(answer)==0:
+            answer.append(i)
+        else:
+            if answer[-1]!=i:
+                answer.append(i)
+    return answer
