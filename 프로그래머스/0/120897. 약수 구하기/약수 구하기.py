6754d2768c1f2be96b1b9f6960dcf3c7e96c789d
songga0 <125133189+songga0@users.noreply.github.com>
2025-12-13 11:33:31 +0900

    [level 0] Title: 약수 구하기, Time: 0.09 ms, Memory: 9.27 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
@@ -0,0 +1,8 @@
+def solution(n):
+    answer = []
+    for i in range(1,int(n**0.5)+1):
+        if n%i==0:
+            answer.append(i)
+            if i*i!=n:
+                answer.append(n//i)   
+    return sorted(answer)
